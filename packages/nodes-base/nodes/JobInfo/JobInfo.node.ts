2a3f648c6e3f74223e2ded228bb34a2614f8a687
jared-the-automator <jared.the.automator@gmail.com>
2025-01-30 10:17:55 -0500

    Update JobInfo node files

--- packages/nodes-base/nodes/JobInfo/JobInfo.node.ts
+++ packages/nodes-base/nodes/JobInfo/JobInfo.node.ts
@@ -1,49 +1,41 @@
-import { INodeType, INodeTypeDescription, NodeConnectionType } from 'n8n-workflow';
+import { INodeType, INodeTypeDescription } from 'n8n-workflow';
 
 export class JobInfo implements INodeType {
-    description: INodeTypeDescription = {
-        displayName: 'Job Info',
-        name: 'jobInfo',
-        group: ['transform'],
-        version: 1,
-        description: 'Get information about a job',
-        defaults: {
-            name: 'Job Info',
-        },
-        inputs: [
-            {
-                type: NodeConnectionType.Main,
-            }
-        ],
-        outputs: [
-            {
-                type: NodeConnectionType.Main,
-            }
-        ],
-        properties: [
-            {
-                displayName: 'Operation',
-                name: 'operation',
-                type: 'string',
-                default: 'Get Job Info',
-                description: 'The operation to perform',
-            },
-        ],
-    };
+description: INodeTypeDescription = {
+displayName: 'Job Info',
+name: 'jobInfo',
+group: ['transform'],
+version: 1,
+description: 'Get information about a job',
+defaults: {
+name: 'Job Info',
+},
+inputs: ['main'],
+outputs: ['main'],
+properties: [
+{
+displayName: 'Operation',
+name: 'operation',
+type: 'string',
+default: 'Get Job Info',
+description: 'The operation to perform',
+},
+],
+};
 
-    async execute() {
-        // Return the job info
-        return [
-            [
-                {
-                    json: {
-                        jobTitle: 'Software Developer',
-                        company: 'Example Corp',
-                        location: 'Remote',
-                        salary: '$100,000',
-                    },
-                },
-            ],
-        ];
-    }
+async execute() {
+// Return the job info
+return [
+[
+{
+json: {
+jobTitle: 'Software Developer',
+company: 'Example Corp',
+location: 'Remote',
+salary: '$100,000',
+},
+},
+],
+];
+}
 }
