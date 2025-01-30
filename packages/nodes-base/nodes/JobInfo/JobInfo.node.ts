ee35b3c2df0fdb1f31d5902dffdc535387cfaac4
jared-the-automator <jared.the.automator@gmail.com>
2025-01-30 10:23:45 -0500

    Update JobInfo node with correct input/output types

--- packages/nodes-base/nodes/JobInfo/JobInfo.node.ts
+++ packages/nodes-base/nodes/JobInfo/JobInfo.node.ts
@@ -1,41 +1,50 @@
-import { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import type { INodeType, INodeTypeDescription } from 'n8n-workflow';
 
 export class JobInfo implements INodeType {
-description: INodeTypeDescription = {
-displayName: 'Job Info',
-name: 'jobInfo',
-group: ['transform'],
-version: 1,
-description: 'Get information about a job',
-defaults: {
-name: 'Job Info',
-},
-inputs: ['main'],
-outputs: ['main'],
-properties: [
-{
-displayName: 'Operation',
-name: 'operation',
-type: 'string',
-default: 'Get Job Info',
-description: 'The operation to perform',
-},
-],
-};
+    description: INodeTypeDescription = {
+        displayName: 'Job Info',
+        name: 'jobInfo',
+        group: ['transform'],
+        version: 1,
+        description: 'Get information about a job',
+        defaults: {
+            name: 'Job Info',
+        },
+        inputs: [{
+            displayName: 'Input',
+            maxConnections: 1,
+            required: true,
+            type: 'main'
+        }],
+        outputs: [{
+            displayName: 'Output',
+            maxConnections: 1,
+            type: 'main'
+        }],
+        properties: [
+            {
+                displayName: 'Operation',
+                name: 'operation',
+                type: 'string',
+                default: 'Get Job Info',
+                description: 'The operation to perform',
+            },
+        ],
+    };
 
-async execute() {
-// Return the job info
-return [
-[
-{
-json: {
-jobTitle: 'Software Developer',
-company: 'Example Corp',
-location: 'Remote',
-salary: '$100,000',
-},
-},
-],
-];
-}
+    async execute() {
+        // Return the job info
+        return [
+            [
+                {
+                    json: {
+                        jobTitle: 'Software Developer',
+                        company: 'Example Corp',
+                        location: 'Remote',
+                        salary: '$100,000',
+                    },
+                },
+            ],
+        ];
+    }
 }
