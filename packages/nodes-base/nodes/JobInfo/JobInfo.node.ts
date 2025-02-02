8f64af95e712ae2a7ec6d2e86c512c64427f793c
jared-the-automator <jared.the.automator@gmail.com>
2025-02-02 13:56:33 -0500

    Add Puppeteer dependency for LinkedIn profile search and format JobInfo node

--- packages/nodes-base/nodes/JobInfo/JobInfo.node.ts
+++ packages/nodes-base/nodes/JobInfo/JobInfo.node.ts
@@ -1,88 +1,101 @@
-import { INodeType, INodeTypeDescription, NodeConnectionType, IExecuteFunctions } from 'n8n-workflow';
+import {
+	INodeType,
+	INodeTypeDescription,
+	NodeConnectionType,
+	IExecuteFunctions,
+} from 'n8n-workflow';
 
 export class JobInfo implements INodeType {
-    description: INodeTypeDescription = {
-        displayName: 'Job Info',
-        name: 'jobInfo',
-        icon: 'fa:briefcase',
-        group: ['transform'],
-        version: 1,
-        description: 'Create and manage job information',
-        defaults: {
-            name: 'Job Info',
-        },
-        inputs: [{
-            displayName: 'Input',
-            maxConnections: 1,
-            required: true,
-            type: NodeConnectionType.Main
-        }],
-        outputs: [{
-            displayName: 'Output',
-            maxConnections: 1,
-            type: NodeConnectionType.Main
-        }],
-        properties: [
-            {
-                displayName: 'Job Title',
-                name: 'jobTitle',
-                type: 'string',
-                default: '',
-                description: 'Title of the job position',
-                required: false,
-            },
-            {
-                displayName: 'Industry',
-                name: 'industry',
-                type: 'string',
-                default: '',
-                description: 'Industry sector of the job',
-                required: false,
-            },
-            {
-                displayName: 'Company',
-                name: 'company',
-                type: 'string',
-                default: '',
-                description: 'Name of the company',
-                required: false,
-            },
-            {
-                displayName: 'Location',
-                name: 'location',
-                type: 'string',
-                default: '',
-                description: 'Location of the job',
-                required: false,
-            }
-        ],
-    };
+	description: INodeTypeDescription = {
+		displayName: 'Job Info',
+		name: 'jobInfo',
+		icon: 'fa:briefcase',
+		group: ['transform'],
+		version: 1,
+		description: 'Create and manage job information',
+		defaults: {
+			name: 'Job Info',
+		},
+		inputs: [
+			{
+				displayName: 'Input',
+				maxConnections: 1,
+				required: true,
+				type: NodeConnectionType.Main,
+			},
+		],
+		outputs: [
+			{
+				displayName: 'Output',
+				maxConnections: 1,
+				type: NodeConnectionType.Main,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Job Title(s)',
+				name: 'jobTitle',
+				type: 'string',
+				default: '',
+				description: 'Title(s) of the job position(s), separate multiple with commas',
+				required: false,
+				placeholder: 'e.g. CEO, Founder, Director',
+			},
+			{
+				displayName: 'Industry(s)',
+				name: 'industry',
+				type: 'string',
+				default: '',
+				description: 'Industry sector(s) of the job(s), separate multiple with commas',
+				required: false,
+				placeholder: 'e.g. Technology, Healthcare, Finance',
+			},
+			{
+				displayName: 'Company(s)',
+				name: 'company',
+				type: 'string',
+				default: '',
+				description: 'Name(s) of the company(s), separate multiple with commas',
+				required: false,
+				placeholder: 'e.g. Apple, Google, Microsoft',
+			},
+			{
+				displayName: 'Location(s)',
+				name: 'location',
+				type: 'string',
+				default: '',
+				description: 'Location(s) of the job(s), separate multiple with commas',
+				required: false,
+				placeholder: 'e.g. New York, Remote, London',
+			},
+		],
+	};
 
-    async execute(this: IExecuteFunctions) {
-        const items = this.getInputData();
-        const returnData = [];
+	async execute(this: IExecuteFunctions) {
+		const items = this.getInputData();
+		const returnData = [];
 
-        for (let i = 0; i < items.length; i++) {
-            const jobInfo: Record<string, string> = {};
-            
-            // Only include parameters that have values
-            const jobTitle = this.getNodeParameter('jobTitle', i, '') as string;
-            if (jobTitle) jobInfo.jobTitle = jobTitle;
-            
-            const industry = this.getNodeParameter('industry', i, '') as string;
-            if (industry) jobInfo.industry = industry;
-            
-            const company = this.getNodeParameter('company', i, '') as string;
-            if (company) jobInfo.company = company;
-            
-            const location = this.getNodeParameter('location', i, '') as string;
-            if (location) jobInfo.location = location;
+		for (let i = 0; i < items.length; i++) {
+			const jobInfo: Record<string, string[]> = {};
 
-            returnData.push({
-                json: jobInfo
-            });
-        }
+			// Get and process parameters, splitting on commas and trimming whitespace
+			const jobTitle = this.getNodeParameter('jobTitle', i, '') as string;
+			if (jobTitle) jobInfo.jobTitles = jobTitle.split(',').map((title) => title.trim());
 
-        return [returnData];
-    }
+			const industry = this.getNodeParameter('industry', i, '') as string;
+			if (industry) jobInfo.industries = industry.split(',').map((ind) => ind.trim());
+
+			const company = this.getNodeParameter('company', i, '') as string;
+			if (company) jobInfo.companies = company.split(',').map((comp) => comp.trim());
+
+			const location = this.getNodeParameter('location', i, '') as string;
+			if (location) jobInfo.locations = location.split(',').map((loc) => loc.trim());
+
+			returnData.push({
+				json: jobInfo,
+			});
+		}
+
+		return [returnData];
+	}
 }
