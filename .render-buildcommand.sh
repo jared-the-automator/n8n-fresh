7f81716f3454b46dd73e665813c4c4c1b6d87054
jared-the-automator <jared.the.automator@gmail.com>
2025-02-03 10:24:04 -0500

    Add Chrome installation to build command

--- .render-buildcommand.sh
+++ .render-buildcommand.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install dependencies and build
 pnpm install --no-frozen-lockfile
-pnpm add -g turbo
 pnpm build
